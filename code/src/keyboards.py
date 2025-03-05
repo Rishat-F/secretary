a600327bedef70c34910704d27c913f7fc6d399d
Rishat Fayzullin <nilluziaf@gmail.com>
2025-03-05 14:56:41 +0300

    dev: просмотр клиентами списка предоставляемых услуг
    Closed #46 task

--- code/src/keyboards.py
+++ code/src/keyboards.py
@@ -3,11 +3,13 @@
 from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 
 SHOW_ID = "Вывести ID"
+USLUGI = "Услуги"
 
 
 main_keyboard = ReplyKeyboardMarkup(
     keyboard=[
         [KeyboardButton(text=SHOW_ID)],
+        [KeyboardButton(text=USLUGI)],
     ],
     resize_keyboard=True,
 )
