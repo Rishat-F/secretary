07bd83cd5bd5ffedad694bf7550c9787205c0bf4
Rishat Fayzullin <nilluziaf@gmail.com>
2025-03-06 11:02:03 +0300

    dev: просмотр услуг поставщиком услуг
    Closed #52 task

--- code/src/handlers.py
+++ code/src/handlers.py
@@ -1,12 +1,14 @@
 """Обработчики бота."""
 
 from aiogram import types
+from aiogram.fsm.context import FSMContext
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
 
 import messages
 from config import ADMIN_TG_ID
 from database import get_uslugi
-from keyboards import main_keyboard
+from keyboards import BACK, SHOW_ALL_USLUGI, main_keyboard, uslugi_keyboard
+from states import UslugiActions
 from utils import form_uslugi_list_text
 
 
@@ -20,11 +22,38 @@ async def show_id(message: types.Message):
     )
 
 
-async def show_uslugi(
+async def _show_uslugi(
     message: types.Message,
     async_session: async_sessionmaker[AsyncSession],
+    keyboard: types.ReplyKeyboardMarkup,
+) -> None:
+    uslugi = await get_uslugi(async_session)
+    text = form_uslugi_list_text(uslugi)
+    await message.answer(text, reply_markup=keyboard)
+
+
+async def uslugi(
+    message: types.Message,
+    async_session: async_sessionmaker[AsyncSession],
+    state: FSMContext,
 ):
     if message.from_user.id != ADMIN_TG_ID:
-        uslugi = await get_uslugi(async_session)
-        text = form_uslugi_list_text(uslugi)
-        await message.answer(text, reply_markup=main_keyboard)
+        await _show_uslugi(message, async_session, main_keyboard)
+    else:
+        await message.answer(messages.CHOOSE_ACTION, reply_markup=uslugi_keyboard)
+        await state.set_state(UslugiActions.choose_action)
+
+
+async def choose_uslugi_action(
+    message: types.Message,
+    async_session: async_sessionmaker[AsyncSession],
+    state: FSMContext,
+) -> None:
+    upper_text = message.text.upper()
+    if upper_text == BACK.upper():
+        await state.clear()
+        await message.answer(messages.MAIN_MENU, reply_markup=main_keyboard)
+    elif upper_text == SHOW_ALL_USLUGI.upper():
+        await _show_uslugi(message, async_session, uslugi_keyboard)
+    else:
+        await message.answer(messages.CHOOSE_GIVEN_ACTION, reply_markup=uslugi_keyboard)
--- code/src/keyboards.py
+++ code/src/keyboards.py
@@ -2,14 +2,26 @@
 
 from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 
+BACK = "Назад"
 SHOW_ID = "Вывести ID"
 USLUGI = "Услуги"
+SHOW_ALL_USLUGI = "Показать все услуги"
 
 
 main_keyboard = ReplyKeyboardMarkup(
     keyboard=[
         [KeyboardButton(text=SHOW_ID)],
         [KeyboardButton(text=USLUGI)],
+    ],
+    resize_keyboard=True,
+)
+
+uslugi_keyboard = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text=BACK),
+            KeyboardButton(text=SHOW_ALL_USLUGI),
+        ],
     ],
     resize_keyboard=True,
 )
