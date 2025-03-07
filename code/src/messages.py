140b93f4c52ab89322d826de56c0d11532693b57
Rishat Fayzullin <nilluziaf@gmail.com>
2025-03-07 17:02:03 +0300

    refactor, dev: косметические правки в модуле src/messages.py

--- code/src/messages.py
+++ code/src/messages.py
@@ -4,8 +4,9 @@ GREETING = "Привет!\nНужно придумать приветствие"
 YOUR_ID = "Твой ID:\n<b>{id}</b>"
 NO_USLUGI = "На данный момент никаких услуг нет"
 CHOOSE_ACTION = "Выберите действие"
-MAIN_MENU = "Главное меню"
 CHOOSE_GIVEN_ACTION = "Выберите действие из предложенных"
+MAIN_MENU = "Главное меню"
+
 SET_USLUGA_NAME = "Задайте название услуги"
 USLUGA_ALREADY_EXISTS = 'Услуга с названием <b>"{name}"</b> уже существует!'
 SET_USLUGA_PRICE = "Задайте цену услуги в рублях\n(например, 300)"
@@ -13,21 +14,25 @@ SET_USLUGA_DURATION = (
     "Задайте длительность услуги в минутах (кратно 30)\n(например, 90)"
 )
 USLUGA_CREATED = 'Услуга <b>"{name}"</b> создана!'
+
 CHOOSE_USLUGA_TO_UPDATE = "Выберите услугу, которую хотите изменить"
 CHOOSE_GIVEN_USLUGI = "Выберите услугу из предложенных"
 CHOOSE_FIELD_TO_CHANGE = "Выберите, что хотите изменить"
 CHOOSE_GIVEN_FIELD_TO_CHANGE = "Выберите из предложенных"
+
 SET_USLUGA_NEW_NAME = "Задайте новое название услуги"
 USLUGA_OLD_NAME_AND_NEW_NAME_ARE_SIMILAR = (
     "Новое название ничем не отличается от старого!"
 )
 USLUGA_NEW_NAME_SETTLED = 'Название "{old_name}" изменено на <b>"{new_name}"</b>'
+
 SET_USLUGA_NEW_PRICE = "Задайте новую цену услуги"
 USLUGA_OLD_PRICE_AND_NEW_PRICE_ARE_SIMILAR = "Новая цена не отличается от старой!"
 USLUGA_NEW_PRICE_SETTLED = (
     'Цена услуги <b>"{usluga_name}"</b> изменена с {old_price} рублей '
     'на <b>{new_price} рублей</b>'
 )
+
 SET_USLUGA_NEW_DURATION = "Задайте новую длительность услуги"
 USLUGA_OLD_DURATION_AND_NEW_DURATION_ARE_SIMILAR = (
     "Новая длительность не отличается от старой!"
@@ -36,5 +41,6 @@ USLUGA_NEW_DURATION_SETTLED = (
     'Длительность услуги <b>"{usluga_name}"</b> изменена с {old_duration} минут '
     'на <b>{new_duration} минут</b>'
 )
+
 CHOOSE_USLUGA_TO_DELETE = "Введите порядковый номер услуги, которую хотите удалить"
 USLUGA_DELETED = 'Услуга <b>"{name}"</b> удалена!'
