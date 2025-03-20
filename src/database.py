2e680263a009c530a56d5853cfffc0ea6a312b46
Rishat Fayzullin <nilluziaf@gmail.com>
2025-03-20 12:32:42 +0300

    fix, dev: поправлена запись клиентом на услугу
    Вывод сообщения, что запись создана, а также
    оповещение поставщика услуг о появлении новой записи
    
    Fixed #106 bug

--- src/database.py
+++ src/database.py
@@ -3,77 +3,62 @@
 from datetime import datetime
 
 from sqlalchemy import delete, select, update
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession
 
 
 from src.models import Usluga, Zapis
 
 
 async def get_uslugi(
-    async_session: async_sessionmaker[AsyncSession],
+    session: AsyncSession,
     filter_by: dict | None = None,
 ) -> list[Usluga]:
     if filter_by is None:
         filter_by = dict()
-    async with async_session() as session:
-        query = select(Usluga).filter_by(**filter_by).order_by(Usluga.name)
-        result = await session.execute(query)
-        uslugi = result.scalars().all()
+    query = select(Usluga).filter_by(**filter_by).order_by(Usluga.name)
+    result = await session.execute(query)
+    uslugi = result.scalars().all()
     return list(uslugi)
 
 
-async def insert_usluga(
-    async_session: async_sessionmaker[AsyncSession],
-    usluga: Usluga,
-) -> None:
-    async with async_session() as session:
-        session.add(usluga)
-        await session.commit()
+async def insert_usluga(session: AsyncSession, usluga: Usluga) -> None:
+    session.add(usluga)
+    await session.commit()
 
 
 async def update_usluga(
-    async_session: async_sessionmaker[AsyncSession],
+    session: AsyncSession,
     usluga_name: str,
     new_values: dict,
 ) -> None:
-    async with async_session() as session:
-        stmt = update(Usluga).where(Usluga.name == usluga_name).values(**new_values)
-        await session.execute(stmt)
-        await session.commit()
+    stmt = update(Usluga).where(Usluga.name == usluga_name).values(**new_values)
+    await session.execute(stmt)
+    await session.commit()
 
 
-async def delete_usluga(
-    async_session: async_sessionmaker[AsyncSession],
-    name: str,
-) -> None:
-    async with async_session() as session:
-        query = delete(Usluga).where(Usluga.name==name)
-        await session.execute(query)
-        await session.commit()
+async def delete_usluga(session: AsyncSession, name: str) -> None:
+    query = delete(Usluga).where(Usluga.name==name)
+    await session.execute(query)
+    await session.commit()
 
 
 async def get_active_zapisi(
-    async_session: async_sessionmaker[AsyncSession],
+    session: AsyncSession,
     filter_by: dict | None = None,
 ) -> list[Zapis]:
     if filter_by is None:
         filter_by = dict()
-    async with async_session() as session:
-        query = (
-            select(Zapis)
-            .filter_by(**filter_by)
-            .where(Zapis.starts_at > datetime.now())
-            .order_by(Zapis.starts_at)
-        )
-        result = await session.execute(query)
-        zapisi = result.scalars().all()
+    query = (
+        select(Zapis)
+        .filter_by(**filter_by)
+        .where(Zapis.starts_at > datetime.now())
+        .order_by(Zapis.starts_at)
+    )
+    result = await session.execute(query)
+    zapisi = result.scalars().all()
     return list(zapisi)
 
 
-async def insert_zapis(
-    async_session: async_sessionmaker[AsyncSession],
-    zapis: Zapis,
-) -> None:
-    async with async_session() as session:
-        session.add(zapis)
-        await session.commit()
+async def insert_zapis(session: AsyncSession, zapis: Zapis) -> None:
+    session.add(zapis)
+    await session.commit()
